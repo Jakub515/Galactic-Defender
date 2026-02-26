490d64edbc7df2e5212f65bb8f5199881e3aa7d0
Jakub515 <jakub515.szczurek@gmail.com>
2026-02-26 20:29:57 +0100

    test

--- main.py
+++ main.py
@@ -1,137 +1,84 @@
 import pygame
-import load_images, space_ship, collisions, music, shoot, radar, ui, level_manager
+import load_images
+import space_ship
 from sky import SpaceBackground
-from event import Event
-from enemy_ship import EnemyManager
-from asteroids import AsteroidManager
-from camera import Camera
 
-# --- A. INICJALIZACJA I KONFIGURACJA ---
 pygame.init()
-pygame.font.init()
 
-FPS = 60
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((1920, 1080 ), pygame.FULLSCREEN)
-cxx, cyy = window.get_size()
+
+window = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
 pygame.display.set_caption("Kosmos")
 
-# --- B. ŁADOWANIE ZASOBÓW (Zintegrowane w module) ---
-image_loader = load_images.ImageLoad()
-# Wszystko dzieje się teraz w jednej linijce
-loaded_space_frames, loaded_space_frames_full, audio_files = image_loader.load_all_assets()
+image_load = load_images.ImageLoad()
 
-# --- C. TWORZENIE OBIEKTÓW ---
-music_obj = music.MusicManager(audio_files)
-events_obj = Event()
-bg = SpaceBackground(cxx, cyy, cxx, cyy, 50)
+space_frames = ["images/space_ships/playerShip1_blue.png","images/space_ships/playerShip1_green.png","images/space_ships/playerShip1_orange.png","images/space_ships/playerShip1_red.png","images/space_ships/playerShip2_blue.png","images/space_ships/playerShip2_green.png","images/space_ships/playerShip2_orange.png","images/space_ships/playerShip2_red.png","images/space_ships/playerShip3_blue.png","images/space_ships/playerShip3_green.png","images/space_ships/playerShip3_orange.png","images/space_ships/playerShip3_red.png","images/space_ships/spaceShips_001.png","images/space_ships/spaceShips_002.png","images/space_ships/spaceShips_003.png","images/space_ships/spaceShips_004.png","images/space_ships/spaceShips_005.png","images/space_ships/spaceShips_006.png","images/space_ships/spaceShips_007.png","images/space_ships/spaceShips_008.png","images/space_ships/spaceShips_009.png","images/space_ships/ufoBlue.png","images/space_ships/ufoGreen.png","images/space_ships/ufoRed.png","images/space_ships/ufoYellow.png"]
+space_parts = []
+audio_files = []
 
-class Game():
-    def __init__(self):
-        self.WORLD_CENTER = pygame.math.Vector2(0, 0)
-        self.WORLD_RADIUS = 25_000
-        self.FADE_ZONE = 2000
-        self.shoot_obj = shoot.Shoot(loaded_space_frames)
-        self.camera = Camera(cxx, cyy, lerp_factor=0.08, offset_scalar=15)
-        self.player_parameters = space_ship.Parameters(loaded_space_frames)
-        self.player = space_ship.SpaceShip(loaded_space_frames, audio_files, cxx, cyy, [0, 0], music_obj, self.shoot_obj,self.player_parameters)
-        self.player_shoot = space_ship.Battle(self.player,loaded_space_frames,audio_files,cxx,cyy,[0,0],music_obj,self.shoot_obj,self.player_parameters)
+loaded_space_frames = []
+for i in space_frames:
+    loaded_space_frames.append(image_load.get_image(i,100))
 
-        self.pola_asteroid = [
-            {"pos": pygame.math.Vector2(0, 0), "radius": 22000, "count": 250},
-            {"pos": pygame.math.Vector2(0, 0), "radius": 4500, "count": 50}
-        ]
-        self.asteroid_manager = AsteroidManager(loaded_space_frames_full, self.pola_asteroid)
-        self.enemy_manager = EnemyManager(loaded_space_frames, self.player, music_obj, 5, self.shoot_obj, self.WORLD_RADIUS, self.asteroid_manager)
+player = space_ship.SpaceShip(loaded_space_frames,space_parts,audio_files,1920,1080)
 
-        self.level_manager = level_manager.LevelManager()
-        self.colision_obj = collisions.Collision(music_obj, cxx, cyy)
-        self.radar_obj = radar.Radar(cxx, cyy, 200, self.WORLD_RADIUS)
-        self.game_controller = ui.GameController(self.player_shoot, events_obj, self.player, cxx, cyy, loaded_space_frames_full, clock, self.level_manager)
-        self.paused = False
-        self.dict = None
-
-    def pause_or_resume(self):
-        self.paused = not self.paused
-
-    def mainloop(self, dt:float):
-        if self.paused:
-            return
-        # 2. Logika (Update)
-        self.level_manager.update()
-        self.game_controller.update(dt)
-        self.player.update(dt)
-        self.player_shoot.update(dt)
-        self.enemy_manager.update(dt)
-        self.asteroid_manager.update(dt, self.player, self.enemy_manager)
-        self.shoot_obj.update()
-        
-        # Fizyka bariery świata
-        self.dist = self.player.player_pos.distance_to(self.WORLD_CENTER)
-        if self.dist > self.WORLD_RADIUS:
-            self.player.player_pos = self.WORLD_CENTER + (self.player.player_pos - self.WORLD_CENTER).normalize() * self.WORLD_RADIUS
-            self.player.velocity *= -0.3
-            self.player.destroy_cause_collision()
-
-        self.colision_obj.check_collisions(self.player_shoot, self.player, self.enemy_manager, self.shoot_obj, self.asteroid_manager, self.level_manager)
-        
-        # Aktualizacja kamery
-        self.camera.update(self.player.player_pos, self.player.velocity)
-
-        # 3. Rysowanie (Draw)
-        # Tło pociąga pozycję kamery dla efektu paralaksy/przesuwania
-
-    def draw(self, window: pygame.Surface):
-        # Wizualna bariera świata (używamy camera.apply, aby krąg był na właściwych współrzędnych)
-        if self.dist > self.WORLD_RADIUS - self.FADE_ZONE:
-            alpha = int(max(0, min(1.0, (self.dist - (self.WORLD_RADIUS - self.FADE_ZONE)) / self.FADE_ZONE)) * 255)
-            rel_center = self.camera.apply(self.WORLD_CENTER)
-            temp_s = pygame.Surface((cxx, cyy), pygame.SRCALPHA)
-            pygame.draw.circle(temp_s, (255, 0, 0, alpha // 4), rel_center, self.WORLD_RADIUS, 500)
-            pygame.draw.circle(temp_s, (255, 50, 50, alpha), rel_center, self.WORLD_RADIUS, 15)
-            window.blit(temp_s, (0, 0))
-
-        # Obiekty świata (używają offsetu kamery)
-        off_x, off_y = self.camera.offset.x, self.camera.offset.y
-        self.shoot_obj.draw(window, off_x, off_y)
-        self.asteroid_manager.draw(window, off_x, off_y)
-        self.enemy_manager.draw(window, off_x, off_y)
-        
-        # Gracz (pozycja przeliczona przez kamerę)
-        p_draw = self.camera.apply(self.player.player_pos)
-        self.player.draw(window, p_draw[0], p_draw[1])
-        self.player_shoot.draw(window, p_draw[0], p_draw[1])
-
-        # UI i Radar (na sztywno do ekranu)
-        self.radar_obj.draw(window, self.player, self.enemy_manager, self.asteroid_manager, dt)
-        self.game_controller.draw(window)
-
-
-game_obj = Game()
-
-# --- D. GŁÓWNA PĘTLA ---
-last_esc_state = False
 running = True
-while running:
-    dt = clock.tick(FPS) / 1000.0
-    events_obj.update()
-    if events_obj.system_exit:
-        running = False
-    current_esc_state = events_obj.key_escape
-    
-    if current_esc_state and not last_esc_state:
-        game_obj.pause_or_resume()
 
-    if events_obj.key_f5:
-        game_obj = Game()
-        
-    last_esc_state = current_esc_state
-    game_obj.mainloop(dt)
-    bg.draw(window, (game_obj.camera.pos.x, game_obj.camera.pos.y))
-    game_obj.draw(window)
+scroll_up = False
+scroll_down = False
+
+WORLD_SIZE = 5000000 # To jest tylko logiczna granica, nie fizyczna powierzchnia
+TILE_WIDTH = 1920
+TILE_HEIGHT = 1080
+
+#bg = SpaceBackground(width=100000, height=100000, screen_width=1920, screen_height=1080,num_stars=10000)
+bg = SpaceBackground(tile_width=TILE_WIDTH, tile_height=TILE_HEIGHT, screen_width=1920, screen_height=1080, num_stars=100)
+player_pos = [WORLD_SIZE // 2, WORLD_SIZE // 2] # Utrzymaj gracza w środku logicznego świata
+player_pos = [5000,5000]
+
+clock = pygame.time.Clock()
+FPS = 60
+
+moving_left = moving_right = moving_up = moving_down = False
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            elif event.key == pygame.K_RIGHT:
+                moving_right = True
+            elif event.key == pygame.K_UP:
+                moving_up = True
+            elif event.key == pygame.K_DOWN:
+                moving_down = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            elif event.key == pygame.K_RIGHT:
+                moving_right = False
+            elif event.key == pygame.K_UP:
+                moving_up = False
+            elif event.key == pygame.K_DOWN:
+                moving_down = False
+
+    # Aktualizacja pozycji gracza
+    if moving_left:
+        player_pos[0] -= 5
+    if moving_right:
+        player_pos[0] += 5
+    if moving_up:
+        player_pos[1] -= 5
+    if moving_down:
+        player_pos[1] += 5
+    
+    bg.draw(window, player_pos)
+    player.draw(window)
+
     pygame.display.flip()
 
-# Wyjście
-music_obj.at_exit()
-pygame.font.quit()
+    clock.tick(FPS)
+
 pygame.quit()
