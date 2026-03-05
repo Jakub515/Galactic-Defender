cb4e78b5b85e69e47fa2e120883d0aa7ab24a6fe
Jakub515 <jakub515.szczurek@gmail.com>
2026-03-05 17:59:27 +0100

    dodanie wielowątkowości do ładowania

--- src/main.py
+++ src/main.py
@@ -23,6 +23,7 @@ def main():
     game_obj = Game(cxx, cyy, gfx_40, gfx_100, audio, music_obj, events_obj, clock)
     running = True
     last_esc_state = False
+    last_f5_state = False
     while running:
         dt = clock.tick(60) / 1000.0 
             
@@ -33,15 +34,25 @@ def main():
             
         # Obsługa pauzy i restartu (F5)
         if events_obj.key_escape and not last_esc_state:
-            game_obj.pause_or_resume()
-        if events_obj.key_f5:
-            game_obj = Game(cxx, cyy, gfx_40, gfx_100, audio, music_obj, events_obj, clock)
+            #game_obj.pause_or_resume(True)
+            pass
+        if events_obj.key_f5 and not last_f5_state:
+            if not game_obj.is_loading:
+                print("Inicjalizacja restartu...")
+                game_obj.start_async_load(game_obj.reset_task)
         
+        # Aktualizacja stanów poprzednich klatek
         last_esc_state = events_obj.key_escape
+        last_f5_state = events_obj.key_f5
 
         # Logika i rysowanie
         game_obj.mainloop(dt)
-        bg.draw(window, (game_obj.camera.pos.x, game_obj.camera.pos.y))
+        try:
+            a = (game_obj.camera.pos.x, game_obj.camera.pos.y)
+            bg.draw(window, a)
+        except:
+            bg.draw(window,(0,0))
+        
         game_obj.draw(window, dt)
         
         pygame.display.flip()
